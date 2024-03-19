3f52766d80cdd4e50dfddda7b8f1040d00fd4af0
TamNhien <canhthien91@gmail.com>
2024-03-19 20:47:28 +0700

    Add project files.

---
+++ P_TinhTuoi.py
@@ -0,0 +1,14 @@
+﻿from datetime import datetime
+
+# Nhập ngày tháng năm sinh
+ngay_sinh = input("Nhập ngày tháng năm sinh (yyyy-mm-dd): ")
+
+# Tính tuổi
+tuoi = datetime.now().year - datetime.strptime(ngay_sinh, '%Y-%m-%d').year
+
+print("Tuổi của bạn là:", tuoi)
+
+from datetime import datetime
+ngay_sinh = input("Nhap ngay thang nam sinh (yyyy-mm-dd): ")
+tuoi = datetime.now().year - datetime.strptime(ngay_sinh, '%Y-%m-%d').year
+print("Tuoi cua ban la : ", tuoi)
